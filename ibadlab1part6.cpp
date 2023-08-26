bda9de296f8e107a74405409fad88a120b167160
VoidPsyker <142975270+VoidPsyker@users.noreply.github.com>
2023-08-26 11:05:32 +0500

    Add files via upload

---
+++ ibadlab1part6.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main (){
+
+int principle;
+float interest;
+int time_period;
+float simple_interest;
+simple_interest=principle*interest*time_period;
+cout<<"principle between 100 to 1000000";
+cin>>principle;
+cout<<"interest rate between 5 to 10%";
+cin>>interest;
+cout<<"time period between 1 to 10 years";
+cin>>time_period;
+cout<<"your simple interest is"<<simple_interest;
+simple_interest=principle*interest*time_period;
+}
